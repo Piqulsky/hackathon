e484fb04eef252138591b08b24befb218a8048f1
Michał Sell <michalsell04@gmail.com>
2023-12-02 04:32:53 +0100

    economy and research panel completed in full without grafics

--- Panels/TreePanelContent/research_panel_manager.gd
+++ Panels/TreePanelContent/research_panel_manager.gd
@@ -18,6 +18,27 @@ var TechnologyButton22 :TextureButton
 var TechnologyButton31 :TextureButton
 var TechnologyButton32 :TextureButton
 
+var Label1 :Label
+var Label2 :Label
+var Label3 :Label
+var Label4 :Label
+var Label5 :Label
+var Label6 :Label
+var Label7 :Label
+var Label8 :Label
+var Label9 :Label
+var Label10 :Label
+
+var MoneyCost1 :Label
+var MoneyCost11 :Label
+var MoneyCost12 :Label
+var MoneyCost2 :Label
+var MoneyCost21 :Label
+var MoneyCost22 :Label
+var MoneyCost3 :Label
+var MoneyCost31 :Label
+var MoneyCost32 :Label
+var MoneyCost4 :Label
 
 func _ready():
 	TechnologyButton1 = $TechnologyButton1
@@ -30,10 +51,38 @@ func _ready():
 	TechnologyButton22 = $TechnologyButton22
 	TechnologyButton31 = $TechnologyButton31
 	TechnologyButton32 = $TechnologyButton32
-	
+	Label1 = $TechnologyButton1/Label1
+	Label2 = $TechnologyButton2/Label2
+	Label3 = $TechnologyButton3/Label3
+	Label4 = $TechnologyButton4/Label4
+	Label5 = $TechnologyButton11/Label5
+	Label6 = $TechnologyButton12/Label6
+	Label7 = $TechnologyButton21/Label7
+	Label8 = $TechnologyButton22/Label8
+	Label9 = $TechnologyButton31/Label9
+	Label10 = $TechnologyButton32/Label10
+	MoneyCost1 = $TechnologyButton1/MoneyCost1
+	MoneyCost11 = $TechnologyButton11/MoneyCost1
+	MoneyCost12 = $TechnologyButton12/MoneyCost1
+	MoneyCost2 = $TechnologyButton2/MoneyCost2
+	MoneyCost21 = $TechnologyButton21/MoneyCost2
+	MoneyCost22 = $TechnologyButton22/MoneyCost2
+	MoneyCost3 = $TechnologyButton3/MoneyCost3
+	MoneyCost31 = $TechnologyButton31/MoneyCost3
+	MoneyCost32 = $TechnologyButton32/MoneyCost3
+	MoneyCost4 = $TechnologyButton4/MoneyCost4
 
-func _process(delta):
-	pass
+func _physics_process(delta):
+	MoneyCost1.text = str(Global.resources.science)+"/"+str(costOfLevel1Tech)
+	MoneyCost11.text = str(Global.resources.science)+"/"+str(costOfLevel1Tech)
+	MoneyCost12.text = str(Global.resources.science)+"/"+str(costOfLevel1Tech)
+	MoneyCost2.text = str(Global.resources.science)+"/"+str(costOfLevel2Tech)
+	MoneyCost21.text = str(Global.resources.science)+"/"+str(costOfLevel2Tech)
+	MoneyCost22.text = str(Global.resources.science)+"/"+str(costOfLevel2Tech)
+	MoneyCost3.text = str(Global.resources.science)+"/"+str(costOfLevel3Tech)
+	MoneyCost31.text = str(Global.resources.science)+"/"+str(costOfLevel3Tech)
+	MoneyCost32.text = str(Global.resources.science)+"/"+str(costOfLevel3Tech)
+	MoneyCost4.text = str(Global.resources.science)+"/"+str(costOflevel4Tech)
 
 #village workforce enrollment 1
 func _on_technology_button_1_down():
@@ -141,3 +190,64 @@ func _on_technology_button_32_down():
 		Global.automaticShelterImprover = true
 		if unlockingTech4 == 3:
 			TechnologyButton4.disabled = false
+
+
+func _on_technology_button_1_mouse_entered():
+	Label1.visible = true
+
+func _on_technology_button_1_mouse_exited():
+	Label1.visible = false
+
+func _on_technology_button_2_mouse_entered():
+	Label2.visible = true
+
+func _on_technology_button_2_mouse_exited():
+	Label2.visible = false
+
+func _on_technology_button_3_mouse_entered():
+	Label3.visible = true
+
+func _on_technology_button_3_mouse_exited():
+	Label3.visible = false
+
+func _on_technology_button_4_mouse_entered():
+	Label4.visible = true
+
+func _on_technology_button_4_mouse_exited():
+	Label4.visible = false
+
+func _on_technology_button_11_mouse_entered():
+	Label5.visible = true
+
+func _on_technology_button_11_mouse_exited():
+	Label5.visible = false
+
+func _on_technology_button_12_mouse_entered():
+	Label6.visible = true
+
+func _on_technology_button_12_mouse_exited():
+	Label6.visible = false
+
+func _on_technology_button_21_mouse_entered():
+	Label7.visible = true
+
+func _on_technology_button_21_mouse_exited():
+	Label7.visible = false
+
+func _on_technology_button_22_mouse_entered():
+	Label8.visible = true
+
+func _on_technology_button_22_mouse_exited():
+	Label8.visible = false
+
+func _on_technology_button_31_mouse_entered():
+	Label9.visible = true
+
+func _on_technology_button_31_mouse_exited():
+	Label9.visible = false
+
+func _on_technology_button_32_mouse_entered():
+	Label10.visible = true
+
+func _on_technology_button_32_mouse_exited():
+	Label10.visible = false
